0aaa4df37e42ccea59674ea1d372cf15739f8d99
U-Zyn Chua <chua@uzyn.com>
2016-08-15 15:25:15 +0800

    Much more efficient refund handler

---
+++ server/component/status-checker.js
@@ -0,0 +1,81 @@
+/**
+ * Handles checking of statuses from Twilio and
+ * processes refund both on blockchain and on db
+ */
+const { Notifier, web3 } = require('../../build/.server/contracts');
+const config = require('config');
+const db = require('./db');
+const sms = require('./sms');
+
+let checkStatusesTimer = null;
+let checkingStatuses = false;
+
+// function declaration (get around JSLint's no use before declare
+let setCheckStatusesTimer = null;
+
+function processRefund(dbRow, usdPrice) {
+  const ethPrice = usdPrice / config.get('server.ethUsd');
+  const weiPrice = web3.toWei(ethPrice, 'ether');
+  return new Promise((resolve, reject) => {
+    Notifier.taskProcessedWithCosting(dbRow.txid, weiPrice, {
+      from: web3.eth.accounts[config.get('ethereum.adminAccount')],
+    }, err => {
+      if (err) {
+        return reject(err);
+      }
+      return db.setFinalPrice(dbRow.taskid, usdPrice, ethPrice);
+    });
+  });
+}
+
+function checkStatuses() {
+  checkingStatuses = true;
+  console.log('[ENQ] Checking statuses of pending SMSes...');
+  let dbRows;
+  db.getAllWithoutPricing().then(rows => {
+    dbRows = rows;
+    console.log(`[ENQ] ${rows.length} pending messages...`);
+    const twilioSids = rows.map(row => row.twilioSid);
+    return sms.statuses(twilioSids);
+  }).then(statuses => {
+    checkingStatuses = false;
+    statuses.forEach(status => {
+      if (status.price_unit !== 'USD') {
+        console.log(`Unknown price unit returned from Twilio – ${status.price_unit}`);
+      }
+      const dbRow = dbRows.find(row => row.twilioSid === status.sid);
+
+      let priceUsd = Math.abs(parseFloat(status.price));
+      if (isNaN(priceUsd)) {
+        priceUsd = 0;
+      }
+      processRefund(dbRow, priceUsd);
+    });
+
+    let interval = 60000; // 1 minute
+    if (dbRows.length === 0) {
+      interval = 15 * 60000; // 15 minutes
+    }
+    setCheckStatusesTimer(interval);
+  }, err => {
+    console.log(err);
+    checkingStatuses = false;
+    setCheckStatusesTimer(60000);
+  });
+}
+
+setCheckStatusesTimer = (interval) => {
+  // Timer is running
+  if (checkingStatuses) {
+    return false;
+  }
+
+  if (checkStatusesTimer) {
+    clearTimeout(checkStatusesTimer);
+  }
+  checkStatusesTimer = setTimeout(checkStatuses, interval);
+  console.log(`[ENQ] Status timer is set to ${interval}ms.`);
+  return true;
+};
+
+module.exports = { setCheckStatusesTimer };
--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@
  * Monitors Notifier contract activities on Ethereum
  * and performs actions accordingly
  */
-const { Notifier, web3 } = require('../build/.server/contracts.js');
-const config = require('config');
+const { Notifier } = require('../build/.server/contracts');
+// const config = require('config');
 const db = require('./component/db');
 const sms = require('./component/sms');
+const statusChecker = require('./component/status-checker');
 
 Notifier.TaskUpdated().watch((err, event) => {
   if (err || !event.args.taskId || !event.args.state) {
@@ -25,6 +26,7 @@ Notifier.TaskUpdated().watch((err, event) => {
       db.msgSent(event.args.taskId, txid, twilioData.sid)
     ).then(() => {
       console.log(`Message sent to ${destination}.`);
+      statusChecker.setCheckStatusesTimer(1000);
     }, promiseErr => {
       // TODO: Return (unwithhold) user's funds
       console.log(promiseErr);
@@ -36,51 +38,8 @@ Notifier.TaskUpdated().watch((err, event) => {
   return true;
 });
 
-function processRefund(dbRow, usdPrice) {
-  const ethPrice = usdPrice / config.get('server.ethUsd');
-  const weiPrice = web3.toWei(ethPrice, 'ether');
-  return new Promise((resolve, reject) => {
-    Notifier.taskProcessedWithCosting(dbRow.txid, weiPrice, {
-      from: web3.eth.accounts[config.get('ethereum.adminAccount')],
-    }, err => {
-      if (err) {
-        return reject(err);
-      }
-      return db.setFinalPrice(dbRow.taskid, usdPrice, ethPrice);
-    });
-  });
-}
 
-const checkStatusesInterval = 10000; // 1 minute
-function checkStatuses() {
-  console.log('[ENQ] Checking statuses of pending SMSes...');
-  let dbRows;
-  db.getAllWithoutPricing().then(rows => {
-    dbRows = rows;
-    console.log(`[ENQ] ${rows.length} pending messages...`);
-    const twilioSids = rows.map(row => row.twilioSid);
-    return sms.statuses(twilioSids);
-  }).then(statuses => {
-    statuses.forEach(status => {
-      if (status.price_unit !== 'USD') {
-        console.log(`Unknown price unit returned from Twilio – ${status.price_unit}`);
-      }
-      const dbRow = dbRows.find(row => row.twilioSid === status.sid);
-
-      let priceUsd = Math.abs(parseFloat(status.price));
-      if (isNaN(priceUsd)) {
-        priceUsd = 0;
-      }
-      processRefund(dbRow, priceUsd);
-    });
-    setTimeout(checkStatuses, checkStatusesInterval);
-  }, err => {
-    console.log(err);
-    setTimeout(checkStatuses, checkStatusesInterval);
-  });
-}
-
-checkStatuses();
+statusChecker.setCheckStatusesTimer(5000);
 
 console.log('\n[ ETH Notifier ]');
 console.log(`Watching smart contract at ${Notifier.address}`);
